bec3cbfd46e421aa296bf17b7ed57843b59a37c2
unknown <maxime.soufer@gmail.com>
2022-05-21 22:09:37 -0400

    add a couple features

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
 
 function App() {
+  const [currentEngine, setCurrentEngine] = useState("text-curie-001");
+
   const [promptData, setPromptData] = useState({
     prompt: "",
     temperature: 0.5,
@@ -21,10 +23,15 @@ function App() {
     setPromptData(input);
   }
 
+  function handleEngineChange(event) {
+    setCurrentEngine(event.target.value);
+  }
+
   async function handleSubmit(event) {
     event.preventDefault();
+    event.target.prompt.value = "";
     let fetchData = await fetch(
-      "https://api.openai.com/v1/engines/text-curie-001/completions",
+      `https://api.openai.com/v1/engines/${currentEngine}/completions`,
       {
         method: "POST",
         headers: {
@@ -41,17 +48,67 @@ function App() {
     ]);
   }
 
+  const examplesArray = [
+    "Hello AI, how are you today?",
+    "Translate this into French: ...",
+    "Summarize this for a second-grade student: ...",
+    "Who is Batman?",
+    "How many pounds are in a kilogram?",
+    "Brainstorm some ideas combining VR and fitness.",
+    "Write a tagline for an ice cream shop.",
+  ];
+
+  const [randomPlaceHolder, setRandomPlaceHolder] = useState(
+    "Hello AI, how are you today?"
+  );
+
+  function changeExamplePlaceholder() {
+    setRandomPlaceHolder(
+      examplesArray[Math.floor(Math.random() * examplesArray.length)]
+    );
+  }
+
+  useEffect(() => {
+    setInterval(changeExamplePlaceholder, 5000);
+  }, []);
+
   return (
     <main>
       <header>
         <h1>Shopify Challenge - Fun with Open AI!</h1>
+        <div id="engine-choice">
+          Change Engine:&nbsp;
+          <select onChange={handleEngineChange}>
+            <option title="Most capable GPT-3 model." value="text-davinci-002">
+              text-davinci-002
+            </option>
+            <option
+              title="Very capable, faster and lower cost than Davinci."
+              value="text-curie-001"
+            >
+              text-curie-001
+            </option>
+            <option
+              title="Capable of straightforward tasks, very fast, and lower cost."
+              value="text-babbage-001"
+            >
+              text-babbage-001
+            </option>
+            <option
+              title="Capable of very simple tasks, fastest and lowest cost."
+              value="text-ada-001"
+            >
+              text-ada-001
+            </option>
+          </select>
+        </div>
       </header>
       <form onSubmit={handleSubmit}>
-        <label>Enter a prompt for the Open AI to complete:</label>
+        <label>Enter a prompt for the Open AI:</label>
         <textarea
           name="prompt"
           onChange={handleChange}
-          placeholder="Enter your prompt"
+          placeholder={`Example: "${randomPlaceHolder}"`}
         />
         <button id="submit-button" type="submit">
           Submit
